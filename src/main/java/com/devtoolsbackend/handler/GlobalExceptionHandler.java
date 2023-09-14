5a5ae9af93291c1bcc5063cdbd307c86a374a3f3
Ilya Simanavets <ilya.simanavets@sedex.com>
2023-09-14 12:53:40 +0200

    add logger

--- src/main/java/com/devtoolsbackend/handler/GlobalExceptionHandler.java
+++ src/main/java/com/devtoolsbackend/handler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.devtoolsbackend.handler;
 
 import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,40 +11,41 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.NoSuchElementException;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NoSuchElementException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handleConstraintViolationException(NoSuchElementException e) {
-        System.out.println(e.getMessage());
+        log.info(e.getMessage());
         return e.getMessage();
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleIllegalArgumentException(IllegalArgumentException e) {
-        System.out.println(e.getMessage());
+        log.warn(e.getMessage());
         return e.getMessage();
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleConstraintViolationException(ConstraintViolationException e) {
-        System.out.println(e.getMessage());
+        log.warn(e.getMessage());
         return e.getMessage();
     }
     
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        System.out.println(e.getMessage());
+        log.warn(e.getMessage());
         return e.getMessage();
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public String handleALLServerExceptions(Exception e) {
-        System.out.println(e.getMessage());
+        log.error(e.getMessage());
         return "Something went wrong";
     }
 }
